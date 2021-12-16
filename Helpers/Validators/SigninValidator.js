89a4da4012870c4a80bba82e23c7aa2d769a2983
Emre Seyhan <emreseyhan05@hotmail.com>
2021-12-16 15:13:29 +0300

    duzenlemeler

--- Helpers/Validators/SigninValidator.js
+++ Helpers/Validators/SigninValidator.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 const SigninSchema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().min(3).required()
+    password: Joi.string().min(8).required()
 });
 
 module.exports = SigninSchema;
--- Helpers/Validators/SignupValidator.js
+++ Helpers/Validators/SignupValidator.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const SignupSchema = Joi.object({
     username: Joi.string().min(3).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(3).required(),
+    password: Joi.string().min(8).required(),
 });
 
 module.exports = SignupSchema;
